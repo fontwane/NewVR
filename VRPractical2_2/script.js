bfbcd86a348ce4416c94e160d8ef2ea8b1f120c8
fontwane <sheaf10@nycstudents.net>
2025-10-20 20:38:04 +0000

    AHHHH I HATE THIS AHHHHHH

--- VRPractical2_2/script.js
+++ VRPractical2_2/script.js
@@ -1,5 +1,18 @@
 
 window.addEventListener("DOMContentLoaded",function() {
-
+    scene = document.querySelector("a-scene"); 
+    sphere = document.querySelector("#car");
+    sphere.roty = 0;
+    shpere.droty = 0;
+    loop();
     
 })
+
+function loop(){
+    shere.roty += sphere.droty;
+    shpere.setAttribut("rotation",{x:0,y:sphere.roty,z:0});
+    Sphere.setAttribute("position", {x:0, y:shpere.roty, z:0});
+    window.requestAnimationFrame(loop);
+
+
+}
