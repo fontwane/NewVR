d63f2afb797a3a143655ba5da25e94827445a4b1
fontwane <sheaf10@nycstudents.net>
2025-10-22 00:56:39 +0000

    FINALLY DONE YAYYYYYY

--- VRPractical2_2/script.js
+++ VRPractical2_2/script.js
@@ -5,6 +5,9 @@ window.addEventListener("DOMContentLoaded",function() {
     sphere = document.querySelector("#pokeball");
     rocket = document.querySelector("#rocket");
     dude = document.querySelector("#dude");
+    sun = this.document.querySelector("#sun");
+    sun.fade = 1;
+    sun.opacity = 0;
   
     loop();
     
@@ -14,19 +17,27 @@ window.addEventListener("DOMContentLoaded",function() {
 })
 
 function loop(){
-    a += 0.1;
-    z -= 0.1;
+    a -= 0.1;
+    z += 1;
+    // c += 1;
     box.setAttribute("rotation", {x:0, y:90, z:0});
-    box.setAttribute("position", {x:z, y:0, z:0});
+    box.setAttribute("position", {x:a, y:0, z:-8});
     
-    sphere.setAttribute("rotation", {x:a, y:0, z:0});
+    sphere.setAttribute("rotation", {x:z, y:0, z:0});
 
     rocket.setAttribute("position", {x:3, y:a, z:-4});
 
-    dude.setAttribute("position", {x:-3, y:0, z:z});
+    dude.setAttribute("scale", {x: 2, y: 2, z: 2 });
+    dude.setAttribute("position", {x:-2, y:1.5, z:-10}); 
 
     
+    sun.opacity+=sun.fade/100;
+    sun.opacity < 1 && sun.setAttribute("opacity", sun.opacity);
+
     window.requestAnimationFrame(loop);
 
 
-}
+}       
+
+
+
