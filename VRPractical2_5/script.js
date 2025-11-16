ac707d9ea7f09ad4100e5611fba4e9f8a1bdf55b
fontwane <sheaf10@nycstudents.net>
2025-11-16 12:54:28 +0000

    almost doneeeee

--- VRPractical2_5/script.js
+++ VRPractical2_5/script.js
@@ -43,4 +43,4 @@ window.addEventListener("DOMContentLoaded",function() {
     */
   }
 
-})
+});
---
+++ VRPractical2_5/wall.js
@@ -0,0 +1,10 @@
+class Wall{
+    constructor(x, y, z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+        this.obj = document.creatElement("a-box");
+        
+    }
+}
